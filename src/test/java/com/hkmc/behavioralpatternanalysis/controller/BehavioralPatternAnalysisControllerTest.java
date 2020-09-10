5b5bd3cdb6ba6c30f0d136da36b04416622a028a
메가존/장성문-국내CCS개발팀/최언부 <H2007126@hkmc.hkmg.global>
2020-09-10 10:25:18 +0900

    [CCSIMP-441] ccsp20 sp-parent, db-core, process-core 버전업에 따른 설정 변경

--- src/test/java/com/hkmc/behavioralpatternanalysis/controller/BehavioralPatternAnalysisControllerTest.java
+++ src/test/java/com/hkmc/behavioralpatternanalysis/controller/BehavioralPatternAnalysisControllerTest.java
@@ -3,7 +3,16 @@ package com.hkmc.behavioralpatternanalysis.controller;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ExtendWith(MockitoExtension.class)
+@EnableAutoConfiguration(exclude = {R2dbcAutoConfiguration.class})
 class BehavioralPatternAnalysisControllerTest {
 
 	@Test
