0eda7747f7137fdbdae9766d052dae77e1739943
kilmmk <kilmmk@hyundai-autoever.com>
2020-12-21 22:53:55 +0900

    SecurityConfig 설정 수정

--- src/main/java/com/hkmc/behavioralpatternanalysis/config/SecurityConfig.java
+++ src/main/java/com/hkmc/behavioralpatternanalysis/config/SecurityConfig.java
@@ -1,18 +1,43 @@
 package com.hkmc.behavioralpatternanalysis.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig{
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().permitAll()
-		.and()
-		.httpBasic().disable()
-		.csrf().disable();
+	private static String ENV_PATH = "/actuator/env";
+	private static String SERVICE_PATH = "/behavioralpatternanalysis/**";
+
+	@Configuration
+	public static class ApplicationConfiguration extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+					.antMatcher(SERVICE_PATH).authorizeRequests()
+					.anyRequest().permitAll()
+					.and()
+					.httpBasic().disable()
+					.csrf().disable();
+		}
 	}
- 
+
+	@Configuration
+	@Profile("prd")
+	@Order(1)
+	public static class ActuatorConfiguration extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+					.antMatcher(ENV_PATH).authorizeRequests()
+					.anyRequest().authenticated()
+					.and()
+					.httpBasic()
+					.and()
+					.csrf().disable();
+		}
+	}
 }
